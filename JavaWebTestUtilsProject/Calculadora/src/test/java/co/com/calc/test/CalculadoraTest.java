f306b7c6c2c15cb93f9e9148709702d136f61cf5
jucoba <jubel.correa@gmail.com>
2014-01-10 14:48:32 -0500

    Se ignora un caso de prueba no implementado

--- JavaWebTestUtilsProject/Calculadora/src/test/java/co/com/calc/test/CalculadoraTest.java
+++ JavaWebTestUtilsProject/Calculadora/src/test/java/co/com/calc/test/CalculadoraTest.java
@@ -2,6 +2,7 @@ package co.com.calc.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -16,6 +17,7 @@ public class CalculadoraTest {
 	
 	
 	@Test
+	@Ignore
 	public void sumar_1y2_debe_dar_3() {
 		assertEquals(3, "No implementado");
 	}
