89765acbbd6c854440f9f4b944de7d133a1ac97f
ryanberrio <ryanberrio@hotmail.com>
2014-04-15 11:00:59 -0500

    Ya tiene IE y JSP

---
+++ JavaWebTestUtilsProject/JavaWebTestUtils/src/main/java/co/com/webtest/config/IEDelegateWebDriverProvider.java
@@ -0,0 +1,41 @@
+package co.com.webtest.config;
+
+import org.jbehave.web.selenium.DelegatingWebDriverProvider;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class IEDelegateWebDriverProvider extends DelegatingWebDriverProvider {
+	
+	
+	
+	private DesiredCapabilities capabilities = null;
+	
+	private IEDelegateWebDriverProvider(DesiredCapabilities capabilties) 
+	{
+		this.capabilities = capabilties;
+	}
+	
+	public static IEDelegateWebDriverProvider createDriverProvider(DesiredCapabilities capabilties){
+		return new IEDelegateWebDriverProvider(capabilties);		
+	}
+
+	
+
+
+
+
+
+	public void initialize() {
+		DesiredCapabilities capabilities = this.capabilities;				
+
+		WebDriver driver = new InternetExplorerDriver(capabilities);
+		
+		delegate.set(driver);
+		
+	}
+
+	
+	
+
+}
